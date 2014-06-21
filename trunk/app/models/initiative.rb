71e81ab6ea9091d5586521927f7b04756c36788e
eng_amr_fathi@hotmail.com <eng_amr_fathi@hotmail.com@526ac377-3a76-2876-068e-962205fe4909>
2014-06-21 12:21:48 +0000

    seacrch done by name & sector & region

--- trunk/app/models/initiative.rb
+++ trunk/app/models/initiative.rb
@@ -11,5 +11,14 @@ class Initiative < ActiveRecord::Base
   
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
-has_and_belongs_to_many :results
+  
+  has_and_belongs_to_many :results
+
+
+  #search scope by title & Region of initiative
+  scope :searchRegion, lambda{|searchRegion| where (["region_id LIKE ?","%#{searchRegion}%"])}
+  scope :searchTitle, lambda{|searchTitle| where (["title LIKE ?","%#{searchTitle}%"])}
+  scope :searchSector, lambda{|searchSector| where (["sector_id LIKE ?","%#{searchSector}%"])}
+
+
 end
