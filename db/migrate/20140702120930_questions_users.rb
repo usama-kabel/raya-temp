050b78e4f9b86c1b4fbcfa2dc8faa69d7b308989
minafarouk18 <minafarouk18@526ac377-3a76-2876-068e-962205fe4909>
2014-07-06 09:38:02 +0000

    change in show initiative

--- db/migrate/20140702120930_questions_users.rb
+++ db/migrate/20140702120930_questions_users.rb
@@ -1,7 +1,7 @@
 class QuestionsUsers < ActiveRecord::Migration
   def change
   	create_table :questions_users do |t|
-      t.integer :questions_id
+      t.integer :question_id
       t.integer :user_id
 	  end
   end
