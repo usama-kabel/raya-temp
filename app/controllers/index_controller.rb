933aa769f0e92e6d7d0d9820a97aa57f555f39f3
mohamed.ibrahim.aitta <mohamed.ibrahim.aitta@gmail.com>
2014-07-11 23:23:09 +0000

    follow and follow friends , initiatives of friends

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,5 +1,18 @@
 class IndexController < ApplicationController
   def index
+  	if params[:friends_ids]
+      @initiatives = []
+      @users = current_user.followed_users
+      @users.each do |user|
+        initiatives = Initiative.where("user_id" => user.id)
+        initiatives.each do |initiative|
+          @initiatives.push(initiative)
+        end
+      end
+      @initiatives = @initiatives.sort_by {|obj| obj.created_at}.reverse.paginate(:page => params[:page], :per_page => 5)
+      @friend_initiative = true
+  	else
   	@initiatives = Initiative.all.order('created_at DESC').paginate(:page => params[:page], :per_page => 5)
+  	end
   end
 end
