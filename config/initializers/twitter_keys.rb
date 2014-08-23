6ac3f2d24aa37d48db314752e008cf6873a44825
eng.usamakabel <eng.usamakabel@gmail.com>
2014-08-23 00:15:15 +0000

    Twitter feeds done

---
+++ config/initializers/twitter_keys.rb
@@ -0,0 +1,6 @@
+$client = Twitter::REST::Client.new do |config|
+  config.consumer_key = 'PlQ5xqrwWT7fUZJvNXvPBKN7Y'
+  config.consumer_secret = '70s2Oq0n7UknZ9NsGhfhNoHhnEtbxITRVcf45v080yPyclkutb'
+  config.access_token = '1621928857-5LjEeW7bDDbWPCA4RVHBMSmp2xynx0qGUYrZ0lI'
+  config.access_token_secret = 'LLAcvpCCUD0GC04m4q6mg6pMoZo8x0UztcVXDweQsEPVI'
+end
